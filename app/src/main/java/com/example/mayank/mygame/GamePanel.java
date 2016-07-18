fb8049821c56bc73cb15bd722ae789e385854d0c
mayankjain95 <mayank.jain875@gmail.com>
2016-07-18 17:24:03 +0530

    missiles added

--- app/src/main/java/com/example/mayank/mygame/GamePanel.java
+++ app/src/main/java/com/example/mayank/mygame/GamePanel.java
@@ -3,35 +3,30 @@ package com.example.mayank.mygame;
 import android.content.Context;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
-import android.view.MotionEvent;
-import android.view.SurfaceHolder;
-import android.view.SurfaceView;
-
-
-import android.content.Context;
-import android.graphics.BitmapFactory;
-import android.graphics.Canvas;
+import android.graphics.Rect;
 import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 
-public class GamePanel extends SurfaceView implements SurfaceHolder.Callback
-{
+public class GamePanel extends SurfaceView implements SurfaceHolder.Callback {
     public static final int WIDTH = 856;
     public static final int HEIGHT = 480;
     public static final int MOVESPEED = -5;
     private long smokeStartTime;
+    private long missileStartTime;
     private MainThread thread;
     private Background bg;
     private Player player;
     private ArrayList<Smokepuff> smoke;
+    private ArrayList<Missile> missiles;
+    private Random rand = new Random();
 
 
-    public GamePanel(Context context)
-    {
+    public GamePanel(Context context) {
         super(context);
 
 
@@ -45,34 +40,37 @@ public class GamePanel extends SurfaceView implements SurfaceHolder.Callback
     }
 
     @Override
-    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height){}
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+    }
 
     @Override
-    public void surfaceDestroyed(SurfaceHolder holder){
+    public void surfaceDestroyed(SurfaceHolder holder) {
         boolean retry = true;
         int counter = 0;
-        while(retry && counter<1000)
-        {
+        while (retry && counter < 1000) {
             counter++;
-            try{thread.setRunning(false);
+            try {
+                thread.setRunning(false);
                 thread.join();
                 retry = false;
 
-            }catch(InterruptedException e){e.printStackTrace();}
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
 
         }
 
     }
 
     @Override
-    public void surfaceCreated(SurfaceHolder holder){
+    public void surfaceCreated(SurfaceHolder holder) {
 
         bg = new Background(BitmapFactory.decodeResource(getResources(), R.drawable.grassbg1));
         player = new Player(BitmapFactory.decodeResource(getResources(), R.drawable.helicopter), 65, 25, 3);
         smoke = new ArrayList<Smokepuff>();
-
-        smokeStartTime=  System.nanoTime();
-
+        missiles = new ArrayList<Missile>();
+        smokeStartTime = System.nanoTime();
+        missileStartTime = System.nanoTime();
 
 
         //we can safely start the game loop
@@ -80,22 +78,18 @@ public class GamePanel extends SurfaceView implements SurfaceHolder.Callback
         thread.start();
 
     }
+
     @Override
-    public boolean onTouchEvent(MotionEvent event)
-    {
-        if(event.getAction()==MotionEvent.ACTION_DOWN){
-            if(!player.getPlaying())
-            {
+    public boolean onTouchEvent(MotionEvent event) {
+        if (event.getAction() == MotionEvent.ACTION_DOWN) {
+            if (!player.getPlaying()) {
                 player.setPlaying(true);
-            }
-            else
-            {
+            } else {
                 player.setUp(true);
             }
             return true;
         }
-        if(event.getAction()==MotionEvent.ACTION_UP)
-        {
+        if (event.getAction() == MotionEvent.ACTION_UP) {
             player.setUp(false);
             return true;
         }
@@ -103,52 +97,91 @@ public class GamePanel extends SurfaceView implements SurfaceHolder.Callback
         return super.onTouchEvent(event);
     }
 
-    public void update()
-    {
-        if(player.getPlaying()) {
+    public void update() {
+        if (player.getPlaying()) {
 
             bg.update();
             player.update();
 
-            long elapsed = (System.nanoTime() - smokeStartTime)/1000000;
-            if(elapsed > 120){
-                smoke.add(new Smokepuff(player.getX(), player.getY()+10));
+            //add missiles on timer
+            long missileElapsed = (System.nanoTime() - missileStartTime) / 1000000;
+            if (missileElapsed > (2000 - player.getScore() / 4)) {
+
+                System.out.println("making missile");
+                //first missile always goes down the middle
+                if (missiles.size() == 0) {
+                    missiles.add(new Missile(BitmapFactory.decodeResource(getResources(), R.drawable.
+                            missile), WIDTH + 10, HEIGHT / 2, 45, 15, player.getScore(), 13));
+                } else {
+
+                    missiles.add(new Missile(BitmapFactory.decodeResource(getResources(), R.drawable.missile),
+                            WIDTH + 10, (int) (rand.nextDouble() * (HEIGHT)), 45, 15, player.getScore(), 13));
+                }
+
+                //reset timer
+                missileStartTime = System.nanoTime();
+            }
+            //loop through every missile and check collision and remove
+            for (int i = 0; i < missiles.size(); i++) {
+                //update missile
+                missiles.get(i).update();
+
+                if (collision(missiles.get(i), player)) {
+                    missiles.remove(i);
+                    player.setPlaying(false);
+                    break;
+                }
+                //remove missile if it is way off the screen
+                if (missiles.get(i).getX() < -100) {
+                    missiles.remove(i);
+                    break;
+                }
+            }
+
+
+            //add smokepuffs on timer
+            long elapsed = (System.nanoTime() - smokeStartTime) / 1000000;
+            if (elapsed > 120) {
+                smoke.add(new Smokepuff(player.getX(), player.getY() + 10));
                 smokeStartTime = System.nanoTime();
             }
 
-            for(int i = 0; i<smoke.size();i++)
-            {
+            for (int i = 0; i < smoke.size(); i++) {
                 smoke.get(i).update();
-                if(smoke.get(i).getX()<-10)
-                {
+                if (smoke.get(i).getX() < -10) {
                     smoke.remove(i);
                 }
             }
         }
     }
+
+    public boolean collision(GameObject a, GameObject b) {
+        if (Rect.intersects(a.getRectangle(), b.getRectangle())) {
+            return true;
+        }
+        return false;
+    }
+
     @Override
-    public void draw(Canvas canvas)
-    {
-        final float scaleFactorX = getWidth()/(WIDTH*1.f);
-        final float scaleFactorY = getHeight()/(HEIGHT*1.f);
+    public void draw(Canvas canvas) {
+        final float scaleFactorX = getWidth() / (WIDTH * 1.f);
+        final float scaleFactorY = getHeight() / (HEIGHT * 1.f);
 
-        if(canvas!=null) {
+        if (canvas != null) {
             final int savedState = canvas.save();
-
 
 
             canvas.scale(scaleFactorX, scaleFactorY);
             bg.draw(canvas);
             player.draw(canvas);
-            for(Smokepuff sp: smoke)
-            {
+            // draw smokepuffs
+            for (Smokepuff sp : smoke) {
                 sp.draw(canvas);
             }
-
-
-
-
-
+            //draw missiles
+            for (Missile m : missiles) {
+                m.draw(canvas);
+            }
 
             canvas.restoreToCount(savedState);
         }
